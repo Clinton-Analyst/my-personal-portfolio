b870562e2c07d4b30c388bd9b27f4d4f9ae2d88e
Clinton-Analyst <92388267+Clinton-Analyst@users.noreply.github.com>
2024-11-13 14:11:28 +0300

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,6 @@
+const menuIcon=document.querySelector("menu-icon");
+const navbar=document.querySelector(".navbar");
+menuIcon.onclick={} => {
+    menuIcon.classlist.toggle("bx-x");
+    navbar.classlist.toggle("active");
+}
